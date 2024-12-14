393d51e80498b3fd35498861720fec5fb564beba
Diego Crisanto <gdcrisanto@gmail.com>
2024-12-14 22:06:08 +0800

    Fix: [remove unused assets]

--- components/vectors/index.tsx
+++ components/vectors/index.tsx
@@ -1,27 +1,13 @@
 import Chevron from './chevron';
 import SocialIcons from './socialIcons';
 import DividerIcons from './dividerIcons';
-import Laurels from './laurel';
-import Arrows from './arrow';
 import CommonIcons from './commonIcons';
-import ProcessIcons from './processIcons';
-import Leaves from './leaves';
-import TasteIcons from './tasteIcons';
-import PcdiLogo from './pcdiLogo';
 import siteLogo from './siteLogo';
-import LakanMan from './lakanMan';
 
 export default {
 	...Chevron,
 	...SocialIcons,
 	...DividerIcons,
-	...Laurels,
-	...Arrows,
 	...CommonIcons,
-	...ProcessIcons,
-	...Leaves,
-	...TasteIcons,
-	...PcdiLogo,
 	...siteLogo,
-	...LakanMan,
 };
