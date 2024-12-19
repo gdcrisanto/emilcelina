bea99244e13d52ec31552bc39aa7a23dc8141dee
Diego Crisanto <gdcrisanto@gmail.com>
2024-12-19 12:04:03 +0800

    update: [venue]

--- lib/constants/venue.ts
+++ lib/constants/venue.ts
@@ -1,7 +1,10 @@
 import venueHero from '../../public/assets/venue_hero.jpeg';
-import venueChurch from '../../public/assets/placeholder_church.jpg';
-import venueReception from '../../public/assets/placeholder_reception.jpg';
+import venueChurch from '../../public/assets/church.jpg';
+import venueReception from '../../public/assets/reception.jpeg';
 import venueWhereTo from '../../public/assets/placeholder_venue_where_to_stay.jpg';
+import anya from '../../public/assets/anya.jpg';
+import twinLakes from '../../public/assets/twin_lakes.jpg';
+import escala from '../../public/assets/escala.jpg';
 
 const VENUE_HERO = {
 	image: venueHero,
@@ -44,19 +47,25 @@ const WHERE_TO_STAY = {
 	image: venueWhereTo,
 	title: 'Where to Stay',
 	subtitle: 'Nearby Hotels',
-	body: 'We have rooms blocked off for wedding guests at a hotel and a guesthouse in the Yvoire-Excenevex area. Mention the Pillai–Chopra wedding when you book to get the group discount.',
+	body: 'We have rooms blocked off for wedding guests at Anya Resorts Tagaytay. Mention the Crisanto–Cano wedding when you book to get the group discount.',
 	hotels: [
 		{
-			name: 'HÔTEL FROMAGE',
-			info: '123 Rue de Démo\n12345 La ville de New York\n+33 06 06 06 06 06',
+			name: 'Anya Resorts',
+			info: 'Buenavista Hills Road,\nBarangay Mag-asawang Ilat,\nTagaytay',
+			image: anya,
+			href: 'https://www.anyaresorts.com/',
 		},
 		{
-			name: 'VILLA LAIT',
-			info: '123 Rue de Démo\n12345 La ville de New York\n+33 06 06 06 06 06',
+			name: 'Twin Lakes Hotel',
+			info: 'Tagaytay-Nasugbu Highway,\nLaurel,\nBatangas',
+			image: twinLakes,
+			href: 'https://www.twinlakeshotel.com.ph/en',
 		},
 		{
-			name: 'VILLA LAIT',
-			info: '123 Rue de Démo\n12345 La ville de New York\n+33 06 06 06 06 06',
+			name: 'Escala Tagaytay',
+			info: 'Poblete Street,\nBarangay Maharlika West,\nTagaytay',
+			image: escala,
+			href: 'https://www.escalatagaytay.com/',
 		},
 	],
 };
--- pages/venue.tsx
+++ pages/venue.tsx
@@ -4,6 +4,8 @@ import Constants from '../lib/constants';
 import PageHero from '../components/page-hero';
 import { ColoredBgWrapper } from '../components/page-sections';
 import Sections from '../components/sections';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Venue() {
 	const { VENUE_HERO, VENUE_ANCHORS, VENUE_MAPS, WHERE_TO_STAY } = Constants;
@@ -25,21 +27,22 @@ export default function Venue() {
 			<PageHero
 				image={VENUE_HERO.image}
 				alt=""
-				size="sm"
+				size="md"
 				bgGradient="none"
 				priority
-				customPosition=" object-[right_-22rem_top_0rem] lg:object-center "
-				className="justify-start bg-center bg-cover bg-no-repeat">
+				customPosition=" object-[right_-26rem_top_0rem] lg:object-center "
+				className="justify-start bg-center bg-cover bg-no-repeat"
+			/>
+
+			<ColoredBgWrapper bgColor="nata">
 				<div className="flex w-full max-w-[1920px] mx-auto">
-					<div className="flex-col flex self-center w-full text-white mx-auto lg:m-20 fade-up">
+					<div className="flex-col flex self-center w-full text-white mx-auto m-10 fade-up">
 						<h1 className="text-[#573319] font-playfair font-semibold text-5xl lg:text-7xl leading-[100%] text-center">
 							{VENUE_HERO.title}
 						</h1>
 					</div>
 				</div>
-			</PageHero>
 
-			<ColoredBgWrapper bgColor="nata">
 				<div className="flex flex-row my-6 w-full fade-up">
 					{VENUE_ANCHORS.map((anchor) => {
 						return (
@@ -62,12 +65,12 @@ export default function Venue() {
 									<div
 										className="w-full lg:w-1/2"
 										id={i % 2 ? 'reception' : 'church'}>
-										<div className="w-full lg:max-w-[300px] mx-auto fade-up">
+										<div className="w-full lg:max-w-[550px] mx-auto fade-up">
 											<PageHero
 												image={venue.image}
 												alt=""
 												size="custom"
-												customSize=" h-[180px] lg:h-[180px]"
+												customSize=" h-[180px] lg:h-[445px]"
 												width={869}
 												height={518}
 											/>
@@ -84,29 +87,18 @@ export default function Venue() {
 				</div>
 			</ColoredBgWrapper>
 
-			<PageHero
-				image={WHERE_TO_STAY.image}
-				alt=""
-				size="xs"
-				bgGradient="none"
-				priority
-				className="justify-start bg-center bg-cover bg-no-repeat">
-				<div className="flex w-full max-w-[1920px] mx-auto fade-up">
-					<div className="flex-col flex self-center w-full text-white mx-auto lg:m-20 fade-up">
-						<h2 className="font-playfair text-[#46542f] text-4xl lg:text-6xl leading-[100%] text-center mb-6">
-							{WHERE_TO_STAY.title}
-						</h2>
-						<h5 className=" text-[#46542f] text-2xl lg:text-2xl leading-[100%] text-center">
-							{WHERE_TO_STAY.subtitle}
-						</h5>
-					</div>
-				</div>
-			</PageHero>
-
 			<ColoredBgWrapper bgColor="white">
 				<div
 					id="reception"
 					className="flex flex-col w-full max-w-[1920px] mx-auto gap-x-20 px-4 lg:px-32 py-10 lg:py-20">
+					<div className="flex w-full max-w-[1920px] mx-auto fade-up">
+						<div className="flex-col flex self-center w-full text-white mx-auto lg:mb-20 fade-up">
+							<h2 className="font-playfair text-[#46542f] text-4xl lg:text-6xl leading-[100%] text-center">
+								{WHERE_TO_STAY.title}
+							</h2>
+						</div>
+					</div>
+
 					<h4 className="text-xl lg:text-2xl leading-[100%] text-center mx-auto max-w-[640px] mb-10 fade-up text-[#46542f]">
 						{WHERE_TO_STAY.body}
 					</h4>
@@ -114,7 +106,25 @@ export default function Venue() {
 					<div className="flex flex-col lg:flex-row w-full max-w-full lg:max-w-1/4 justify-around text-[#573319] fade-up gap-y-6">
 						{WHERE_TO_STAY.hotels.map((hotel) => {
 							return (
-								<div className="flex flex-col  w-full lg:w-1/4">
+								<div className="flex flex-col  w-full lg:w-1/4 self-end">
+									<div
+										className={`flex-col items-center w-full relative fade-up flex-grow max-w-full mb-4`}>
+										<div className=" flex relative w-full h-[200px] lg:h-[200px] max-w-full">
+											<Link
+												className="flex text-[#FCF4EA] capitalize z-20 absolute w-full h-full items-end p-2 text-2xl"
+												href={hotel.href}
+												target="_blank"
+											/>
+											<Image
+												src={hotel.image}
+												alt=""
+												width={422}
+												height={300}
+												placeholder="blur"
+												className="absolute object-cover object-bottom w-full h-[287px] lg:h-[200px] max-h-[200px] z-[1]"
+											/>
+										</div>
+									</div>
 									<h3 className="leading-[100%] text-center mb-4 text-2xl lg:text-[2.125rem] text-[#46542f]">
 										{hotel.name}
 									</h3>
