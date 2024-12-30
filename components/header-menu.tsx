d5250ed3cdd96c4572242301e127abd2039225cf
gdcrisanto <gdcrisanto@gmail.com>
2024-12-30 18:28:11 +0800

    fix: [header]

--- components/header-menu.tsx
+++ components/header-menu.tsx
@@ -15,9 +15,9 @@ export default function HeaderMenu({
 
 	useEffect(() => {
 		if (isOpen) {
-			document.body.classList.add('overflow-y-hidden');
+			document.body.style.overflow = 'hidden';
 		} else {
-			document.body.classList.remove('overflow-y-hidden');
+			document.body.style.overflow = 'unset';
 		}
 	}, [isOpen]);
 	return (
--- components/sections/siteHeader.tsx
+++ components/sections/siteHeader.tsx
@@ -54,7 +54,7 @@ const SiteHeader = ({ textBlack }: SiteHeaderProps) => {
 		<div
 			onScroll={changeNavBg}
 			className={
-				`z-50 h-[72px] lg:h-[127px] w-full mx-auto fixed bg-[#FCF4EA] transition-all ease-in-out duration-300 lg:duration-200 ${!isScrollUp ? 'top-[-72px] lg:top-[-172px]' : 'top-0'} ${isOpen ? 'site-header-scroll' : ''}`
+				`z-50 h-[72px] lg:h-[127px] w-full mx-auto fixed bg-[#FCF4EA] transition-all ease-in-out duration-300 lg:duration-200 ${(isScrollUp || isOpen) ? 'top-0' : 'top-[-72px] lg:top-[-172px]'} ${isOpen ? 'site-header-scroll' : ''}`
 			}
 		>
 			<div className="flex z-50 flex-row container mx-auto h-full px-6 justify-between capitalize font-normal items-center">
