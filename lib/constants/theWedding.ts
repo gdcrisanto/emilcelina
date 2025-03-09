6190f3737707b83197afa599465b2610c72344fa
Diego Crisanto <gdcrisanto@gmail.com>
2025-03-09 17:22:40 +0800

    feat: add invitation section

--- lib/constants/theWedding.ts
+++ lib/constants/theWedding.ts
@@ -1,195 +1,205 @@
 import weddingHero from '../../public/assets/wedding_hero.jpeg';
+import invitation1 from '../../public/assets/invitation1.png';
+import invitation2 from '../../public/assets/invitation2.png';
+import invitation3 from '../../public/assets/invitation3.jpeg';
 
 const WEDDING_HERO = {
-	image: weddingHero,
-	title: 'Wedding Details',
+  image: weddingHero,
+  title: 'Wedding Details',
 };
 
 const WEDDING_ANCHORS = [
-	{
-		title: 'Schedule',
-		id: 'schedule',
-	},
-	{
-		title: 'Attire',
-		id: 'attire',
-	},
-	// {
-	// 	title: 'Entourage',
-	// 	id: 'entourage',
-	// },
+  {
+    title: 'Invitation',
+    id: 'invitation',
+  },
+  {
+    title: 'Schedule',
+    id: 'schedule',
+  },
+  {
+    title: 'Attire',
+    id: 'attire',
+  },
+  // {
+  // 	title: 'Entourage',
+  // 	id: 'entourage',
+  // },
 ];
 
 const SCHEDULE_OF_EVENTS = {
-	title: 'Schedule of Events',
-	events: [
-		{
-			event: 'Arrival',
-			description: 'Our Lady of Lourdes Parish',
-			time: '3:00 PM',
-		},
-		{
-			event: 'Ceremony',
-			description: 'Our Lady of Lourdes Parish',
-			time: '3:30 PM',
-		},
-		{
-			event: 'Cocktails',
-			description: 'The Lobby at Arocarrìa',
-			time: '5:30 PM',
-		},
-		{
-			event: 'Dinner',
-			description: 'The Ballroom at Arocarrìa',
-			time: '7:00 PM',
-		},
-		{
-			event: 'After-Party',
-			description: 'The Ballroom at Arocarrìa',
-			time: '9:00 PM',
-		},
-	],
+  title: 'Schedule of Events',
+  events: [
+    {
+      event: 'Arrival',
+      description: 'Our Lady of Lourdes Parish',
+      time: '3:00 PM',
+    },
+    {
+      event: 'Ceremony',
+      description: 'Our Lady of Lourdes Parish',
+      time: '3:30 PM',
+    },
+    {
+      event: 'Cocktails',
+      description: 'The Lobby at Arocarrìa',
+      time: '5:30 PM',
+    },
+    {
+      event: 'Dinner',
+      description: 'The Ballroom at Arocarrìa',
+      time: '7:00 PM',
+    },
+    {
+      event: 'After-Party',
+      description: 'The Ballroom at Arocarrìa',
+      time: '9:00 PM',
+    },
+  ],
 };
 
 const ATTIRE_FOR_GUESTS = {
-	title: 'Attire for Guests',
-	ladies: {
-		title: 'Ladies',
-		description: 'Floor-length gown in beige, ecru or taupe',
-		subDescription: 'Please reserve white for the bride.',
-	},
-	gentlemen: {
-		title: 'Gentlemen',
-		description: 'Long-sleeved Barong Tagalog with black trousers',
-		subDescription: 'We kindly request classic beige for your Barong Tagalog.',
-	},
-	colors: ['bg-[#F0E8DB]', 'bg-[#E2D4C5]', 'bg-[#D8C9BA]'],
-	note: 'Dress code is strictly formal.',
+  title: 'Attire for Guests',
+  ladies: {
+    title: 'Ladies',
+    description: 'Floor-length gown in beige, ecru or taupe',
+    subDescription: 'Please reserve white for the bride.',
+  },
+  gentlemen: {
+    title: 'Gentlemen',
+    description: 'Long-sleeved Barong Tagalog with black trousers',
+    subDescription: 'We kindly request classic beige for your Barong Tagalog.',
+  },
+  colors: ['bg-[#F0E8DB]', 'bg-[#E2D4C5]', 'bg-[#D8C9BA]'],
+  note: 'Dress code is strictly formal.',
 };
 
 const ENTOURAGE = {
-	title: 'Entourage',
-	sections: [
-		{
-			title: 'PRINCIPAL SPONSORS',
-			names: [
-				{
-					left: 'Angel Emperado-Uy',
-					right: 'Antonio Catangui',
-				},
-				{
-					left: 'Jing Turalba',
-					right: 'Bernie Fontanilla',
-				},
-				{
-					left: 'Jo Catangui',
-					right: 'Dindo Campilan',
-				},
-				{
-					left: 'Rence Bascuña',
-					right: 'Jorge Guerrero',
-				},
-				{
-					left: 'Rhodora Kapunan',
-					right: 'Robert Cano',
-				},
-				{
-					left: 'Rose Mercado',
-					right: 'Tim Abejo',
-				},
-			],
-		},
-		{
-			title: {
-				left: 'BEST MEN',
-				right: 'MAID & MATRON OF HONOR',
-			},
-			names: [
-				{
-					left: 'Emilio Cano IV',
-					right: 'Janine Marie Crisanto',
-				},
-				{
-					left: 'Gerald Lumba',
-					right: 'Christine Anne Crisanto',
-				},
-			],
-		},
-		{
-			title: {
-				left: 'GROOMSMEN',
-				right: 'BRIDESMAIDS',
-			},
-			names: [
-				{
-					left: 'Elijah Corpus',
-					right: 'Anna Dominique Yao',
-				},
-				{
-					left: 'Josefino Catangui',
-					right: 'Jessica Rae Siapno',
-				},
-				{
-					left: 'JV Valerio',
-					right: 'Julia Granada',
-				},
-				{
-					left: 'Rafa Abaya',
-					right: 'Samantha Mae Mercado',
-				},
-				{
-					left: 'Ricky Fontanilla',
-					right: 'Viktor Kristian Kapunan',
-				},
-			],
-		},
-		{
-			title: {
-				left: 'CANDLE',
-				right: 'VEIL',
-			},
-			names: [
-				{
-					left: 'Gian Lorenzo Crisanto',
-					right: 'Gian Paolo Crisanto',
-				},
-				{
-					left: 'Cyril Catherine Cano',
-					right: 'Annie Concepcion',
-				},
-			],
-		},
-		{
-			title: 'CORD',
-			names: ['Gian Diego Crisanto', 'Lorem Ipsum'],
-		},
-		{
-			title: {
-				left: 'COIN BEARER',
-				right: 'BIBLE BEARER',
-			},
-			names: [
-				{
-					left: 'Lorem Ipsum',
-					right: 'Lorem Ipsum',
-				},
-			],
-		},
-		{
-			title: 'RING BEARER',
-			names: 'Lorem Ipsum',
-		},
-		{
-			title: 'FLOWER GIRLS',
-			names: ['Penelope Rose Crisanto', 'Sunny Alican', 'Tori Hu'],
-		},
-	],
+  title: 'Entourage',
+  sections: [
+    {
+      title: 'PRINCIPAL SPONSORS',
+      names: [
+        {
+          left: 'Angel Emperado-Uy',
+          right: 'Antonio Catangui',
+        },
+        {
+          left: 'Jing Turalba',
+          right: 'Bernie Fontanilla',
+        },
+        {
+          left: 'Jo Catangui',
+          right: 'Dindo Campilan',
+        },
+        {
+          left: 'Rence Bascuña',
+          right: 'Jorge Guerrero',
+        },
+        {
+          left: 'Rhodora Kapunan',
+          right: 'Robert Cano',
+        },
+        {
+          left: 'Rose Mercado',
+          right: 'Tim Abejo',
+        },
+      ],
+    },
+    {
+      title: {
+        left: 'BEST MEN',
+        right: 'MAID & MATRON OF HONOR',
+      },
+      names: [
+        {
+          left: 'Emilio Cano IV',
+          right: 'Janine Marie Crisanto',
+        },
+        {
+          left: 'Gerald Lumba',
+          right: 'Christine Anne Crisanto',
+        },
+      ],
+    },
+    {
+      title: {
+        left: 'GROOMSMEN',
+        right: 'BRIDESMAIDS',
+      },
+      names: [
+        {
+          left: 'Elijah Corpus',
+          right: 'Anna Dominique Yao',
+        },
+        {
+          left: 'Josefino Catangui',
+          right: 'Jessica Rae Siapno',
+        },
+        {
+          left: 'JV Valerio',
+          right: 'Julia Granada',
+        },
+        {
+          left: 'Rafa Abaya',
+          right: 'Samantha Mae Mercado',
+        },
+        {
+          left: 'Ricky Fontanilla',
+          right: 'Viktor Kristian Kapunan',
+        },
+      ],
+    },
+    {
+      title: {
+        left: 'CANDLE',
+        right: 'VEIL',
+      },
+      names: [
+        {
+          left: 'Gian Lorenzo Crisanto',
+          right: 'Gian Paolo Crisanto',
+        },
+        {
+          left: 'Cyril Catherine Cano',
+          right: 'Annie Concepcion',
+        },
+      ],
+    },
+    {
+      title: 'CORD',
+      names: ['Gian Diego Crisanto', 'Lorem Ipsum'],
+    },
+    {
+      title: {
+        left: 'COIN BEARER',
+        right: 'BIBLE BEARER',
+      },
+      names: [
+        {
+          left: 'Lorem Ipsum',
+          right: 'Lorem Ipsum',
+        },
+      ],
+    },
+    {
+      title: 'RING BEARER',
+      names: 'Lorem Ipsum',
+    },
+    {
+      title: 'FLOWER GIRLS',
+      names: ['Penelope Rose Crisanto', 'Sunny Alican', 'Tori Hu'],
+    },
+  ],
 };
 
+const INVITATION_IMAGES = [invitation1, invitation2, invitation3];
+
 export default {
-	WEDDING_HERO,
-	WEDDING_ANCHORS,
-	SCHEDULE_OF_EVENTS,
-	ATTIRE_FOR_GUESTS,
-	ENTOURAGE,
+  WEDDING_HERO,
+  WEDDING_ANCHORS,
+  SCHEDULE_OF_EVENTS,
+  ATTIRE_FOR_GUESTS,
+  ENTOURAGE,
+  INVITATION_IMAGES,
 };
--- pages/the-wedding.tsx
+++ pages/the-wedding.tsx
@@ -4,187 +4,204 @@ import Constants from '../lib/constants';
 import PageHero from '../components/page-hero';
 import { ColoredBgWrapper } from '../components/page-sections';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function TheWedding() {
-	const [isAnchor, setIsAnchor] = useState<any>('schedule')
-	const handleAnchorClick = (anchor: any) => {
-		setIsAnchor(anchor)
-	}
-	const {
-		WEDDING_HERO,
-		WEDDING_ANCHORS,
-		SCHEDULE_OF_EVENTS,
-		ATTIRE_FOR_GUESTS,
-		ENTOURAGE,
-	} = Constants;
+  const [isAnchor, setIsAnchor] = useState<any>('schedule');
+  const handleAnchorClick = (anchor: any) => {
+    setIsAnchor(anchor);
+  };
+  const {
+    WEDDING_HERO,
+    WEDDING_ANCHORS,
+    SCHEDULE_OF_EVENTS,
+    ATTIRE_FOR_GUESTS,
+    INVITATION_IMAGES,
+  } = Constants;
 
-	const scrolltoHash = function (element_id: string) {
-		const element = document.getElementById(element_id);
-		const yOffset = element_id === 'schedule' ? -190 : -150;
-		const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
-		window.scrollTo({top: y, behavior: 'smooth'});
-	};
+  const scrolltoHash = function (element_id: string) {
+    const element = document.getElementById(element_id);
+    const yOffset = element_id === 'schedule' ? -190 : -150;
+    const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  };
 
-	const [isScrollUp, setIsScrollUp] = useState(true);
-	
-		useEffect(() => {
-		const threshold = 0;
-		let lastScrollY = window.scrollY;
-		let ticking = false;
-	
-		const updateScrollDir = () => {
-			const scrollY = window.scrollY;
-	
-			if (Math.abs(scrollY - lastScrollY) < threshold) {
-			ticking = false;
-			return;
-			}
-			setIsScrollUp(scrollY > lastScrollY ? false : true);
-			lastScrollY = scrollY > 0 ? scrollY : 0;
-			ticking = false;
-		};
-	
-		const onScroll = () => {
-			if (!ticking) {
-			window.requestAnimationFrame(updateScrollDir);
-			ticking = true;
-			}
-		};
-	
-		window.addEventListener("scroll", onScroll);
-	
-		return () => window.removeEventListener("scroll", onScroll);
-		}, [isScrollUp]);
+  const [isScrollUp, setIsScrollUp] = useState(true);
 
+  useEffect(() => {
+    const threshold = 0;
+    let lastScrollY = window.scrollY;
+    let ticking = false;
 
-	return (
-		<Layout>
-			<Head>
-				<title>{`${Constants.CMS_NAME}`}</title>
-			</Head>
-			<PageHero
-				image={WEDDING_HERO.image}
-				alt=""
-				size="custom"
-				bgGradient="none"
-				priority
-				className="justify-start bg-center bg-cover bg-no-repeat mt-[72px] lg:mt-[127px]"
-				customSize=" h-[calc(80svh-72px)] lg:h-[calc(78svh-127px)]"
-			/>
+    const updateScrollDir = () => {
+      const scrollY = window.scrollY;
 
-			<ColoredBgWrapper bgColor="nata"> 
-				
-				<div className="flex w-full max-w-[1920px] mx-auto">
-					<div className="flex-col flex self-center w-full text-white mx-auto m-10 fade-up">
-						<h1 className="text-[#573319] font-playfair font-semibold text-5xl lg:text-7xl leading-[100%] text-center">
-							{WEDDING_HERO.title}
-						</h1>
-					</div>
-				</div>
-				</ColoredBgWrapper>
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setIsScrollUp(scrollY > lastScrollY ? false : true);
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
 
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
 
-				<div className={`flex flex-row py-4 w-full text-base lg:text-2xl z-30 text-[#46542f] px-4 justify-around sticky transition-all ease-in-out duration-300 ${!isScrollUp ? 'top-0' : 'top-[72px] lg:top-[127px]' } bg-[#FCF4EA]`}>
-					{WEDDING_ANCHORS.map((anchor) => {
-						return (
-							<a
-								className={`w-1/4 text-center cursor-pointer ${isAnchor === anchor.id ? 'border-b border-[#46542f]' : ''}`}
-								onClick={() => {
-									scrolltoHash(anchor.id);
-									handleAnchorClick(anchor.id)
-								}}>
-								{anchor.title}
-							</a>
-						);
-					})}
-			</div>
-				<ColoredBgWrapper bgColor="nata"> 
-				<div id="schedule"/>
-				<div
-					className="flex flex-col lg:flex-row w-full max-w-[1920px] mx-auto gap-x-20 px-10 lg:px-32 py-14">
-					<div className="flex flex-col w-full lg:w-[48%] mb-20 lg:mb-0 justify-center ">
-						<h2 className="font-playfair text-4xl text-center fade-up text-[#573319] font-playfair lg:text-[3rem] leading-[100%] font-medium">
-							{SCHEDULE_OF_EVENTS.title}
-						</h2>
-						
-					</div>
-					<div className="flex flex-col lg:max-w-[400px] w-full text-[#5F6F52] fade-up">
-						{SCHEDULE_OF_EVENTS?.events.map((item) => {
-							return (
-								<div className="flex flex-row mb-16 justify-between">
-									<div className="flex flex-col">
-										<h3 className="text-2xl lg:text-[2.125rem] text-[#46542f]">
-											{item.event}
-										</h3>
-										<span className="text-base italic text-[#573319]">
-											{item.description}
-										</span>
-									</div>
-									<div>
-										<h4 className="text-xl lg:text-2xl text-[#46542f]">
-											{item.time}
-										</h4>
-									</div>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			</ColoredBgWrapper>
+    window.addEventListener('scroll', onScroll);
 
-			<ColoredBgWrapper bgColor="white">
-				<div id="attire"/>
-				<div
-					className="flex flex-col-reverse lg:flex-row w-full max-w-[1920px] mx-auto gap-x-20 px-10 lg:px-32 py-10 pt-20 lg:py-20 fade-up">
-					<div className="flex flex-col w-full lg:w-[55%] mb-20 lg:mb-0 justify-center ">
-						<div className="flex flex-col-reverse w-full">
-							<div className="flex flex-col w-full  mb-10 lg:mb-10 justify-center ">
-								<h3 className="leading-[100%] text-center mb-6 text-2xl lg:text-[2.125rem] text-[#46542f]">
-									{ATTIRE_FOR_GUESTS.ladies.title}
-								</h3>
-								<h4 className="text-center text-xl text-[#573319] mb-1">
-									{ATTIRE_FOR_GUESTS.ladies.description}
-								</h4>
-								<span className="text-base italic text-center text-[#573319]">
-									{ATTIRE_FOR_GUESTS.ladies.subDescription}
-								</span>
-							</div>
-							<div className="flex flex-col w-full mb-10 lg:mb-20 justify-center ">
-								<h3 className="leading-[100%] text-center mb-6 text-2xl lg:text-[2.125rem] text-[#46542f]">
-									{ATTIRE_FOR_GUESTS.gentlemen.title}
-								</h3>
-								<h4 className="text-center text-xl text-[#573319] mb-1">
-									{ATTIRE_FOR_GUESTS.gentlemen.description}
-								</h4>
-								<span className="text-base italic text-center text-[#573319]">
-									{ATTIRE_FOR_GUESTS.gentlemen.subDescription}
-								</span>
-							</div>
-						</div>
-						<div className="flex flex-row w-full justify-around my-6">
-							{ATTIRE_FOR_GUESTS.colors.map((color, i) => {
-								const colors = [
-									'bg-[#F0E8DB]',
-									'bg-[#E2D4C5]',
-									'bg-[#D8C9BA]',
-								];
-								const className = `${colors[i]} w-1/4 pt-[25%]`;
-								return <div className={className} />;
-							})}
-						</div>
-						<span className="text-base italic text-center text-[#573319]">
-							{ATTIRE_FOR_GUESTS.note}
-						</span>
-					</div>
-					<div className="flex flex-col w-full lg:w-[45%] mb-20 lg:mb-0 justify-center ">
-						<h2 className="font-playfair text-center fade-up text-[#573319] font-playfair text-4xl lg:text-[3rem] leading-[100%]">
-							{ATTIRE_FOR_GUESTS.title}
-						</h2>
-					</div>
-				</div>
-			</ColoredBgWrapper>
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [isScrollUp]);
 
-			{/* <ColoredBgWrapper bgColor="nata">
+  return (
+    <Layout>
+      <Head>
+        <title>{`${Constants.CMS_NAME}`}</title>
+      </Head>
+      <PageHero
+        image={WEDDING_HERO.image}
+        alt=""
+        size="custom"
+        bgGradient="none"
+        priority
+        className="justify-start bg-center bg-cover bg-no-repeat mt-[72px] lg:mt-[127px]"
+        customSize=" h-[calc(80svh-72px)] lg:h-[calc(78svh-127px)]"
+      />
+
+      <ColoredBgWrapper bgColor="nata">
+        <div className="flex w-full max-w-[1920px] mx-auto">
+          <div className="flex-col flex self-center w-full text-white mx-auto m-10 fade-up">
+            <h1 className="text-[#573319] font-playfair font-semibold text-5xl lg:text-7xl leading-[100%] text-center">
+              {WEDDING_HERO.title}
+            </h1>
+          </div>
+        </div>
+      </ColoredBgWrapper>
+
+      <div
+        className={`flex flex-row py-4 w-full text-base lg:text-2xl z-30 text-[#46542f] px-4 justify-around sticky transition-all ease-in-out duration-300 ${
+          !isScrollUp ? 'top-0' : 'top-[72px] lg:top-[127px]'
+        } bg-[#FCF4EA]`}
+      >
+        {WEDDING_ANCHORS.map((anchor) => {
+          return (
+            <a
+              className={`w-1/4 text-center cursor-pointer ${
+                isAnchor === anchor.id ? 'border-b border-[#46542f]' : ''
+              }`}
+              onClick={() => {
+                scrolltoHash(anchor.id);
+                handleAnchorClick(anchor.id);
+              }}
+            >
+              {anchor.title}
+            </a>
+          );
+        })}
+      </div>
+
+      <ColoredBgWrapper bgColor="white">
+        <div id="invitation" />
+        <div className="flex flex-col w-full max-w-[1920px] mx-auto gap-x-20 px-60 lg:px-80 py-10 pt-20 lg:py-20 fade-up gap-4">
+          {INVITATION_IMAGES.map((image) => (
+            <Link
+              href="https://drive.google.com/file/d/1ppWEiowsGwemmhHKnolRVnA-lHsQulQc/view?usp=sharing"
+              target="_blank"
+            >
+              <Image
+                src={image}
+                alt="Invite Image"
+              />
+            </Link>
+          ))}
+        </div>
+      </ColoredBgWrapper>
+
+      <ColoredBgWrapper bgColor="nata">
+        <div id="schedule" />
+        <div className="flex flex-col lg:flex-row w-full max-w-[1920px] mx-auto gap-x-20 px-10 lg:px-32 py-14">
+          <div className="flex flex-col w-full lg:w-[48%] mb-20 lg:mb-0 justify-center ">
+            <h2 className="font-playfair text-4xl text-center fade-up text-[#573319] font-playfair lg:text-[3rem] leading-[100%] font-medium">
+              {SCHEDULE_OF_EVENTS.title}
+            </h2>
+          </div>
+          <div className="flex flex-col lg:max-w-[400px] w-full text-[#5F6F52] fade-up">
+            {SCHEDULE_OF_EVENTS?.events.map((item) => {
+              return (
+                <div className="flex flex-row mb-16 justify-between">
+                  <div className="flex flex-col">
+                    <h3 className="text-2xl lg:text-[2.125rem] text-[#46542f]">
+                      {item.event}
+                    </h3>
+                    <span className="text-base italic text-[#573319]">
+                      {item.description}
+                    </span>
+                  </div>
+                  <div>
+                    <h4 className="text-xl lg:text-2xl text-[#46542f]">
+                      {item.time}
+                    </h4>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </ColoredBgWrapper>
+
+      <ColoredBgWrapper bgColor="white">
+        <div id="attire" />
+        <div className="flex flex-col-reverse lg:flex-row w-full max-w-[1920px] mx-auto gap-x-20 px-10 lg:px-32 py-10 pt-20 lg:py-20 fade-up">
+          <div className="flex flex-col w-full lg:w-[55%] mb-20 lg:mb-0 justify-center ">
+            <div className="flex flex-col-reverse w-full">
+              <div className="flex flex-col w-full  mb-10 lg:mb-10 justify-center ">
+                <h3 className="leading-[100%] text-center mb-6 text-2xl lg:text-[2.125rem] text-[#46542f]">
+                  {ATTIRE_FOR_GUESTS.ladies.title}
+                </h3>
+                <h4 className="text-center text-xl text-[#573319] mb-1">
+                  {ATTIRE_FOR_GUESTS.ladies.description}
+                </h4>
+                <span className="text-base italic text-center text-[#573319]">
+                  {ATTIRE_FOR_GUESTS.ladies.subDescription}
+                </span>
+              </div>
+              <div className="flex flex-col w-full mb-10 lg:mb-20 justify-center ">
+                <h3 className="leading-[100%] text-center mb-6 text-2xl lg:text-[2.125rem] text-[#46542f]">
+                  {ATTIRE_FOR_GUESTS.gentlemen.title}
+                </h3>
+                <h4 className="text-center text-xl text-[#573319] mb-1">
+                  {ATTIRE_FOR_GUESTS.gentlemen.description}
+                </h4>
+                <span className="text-base italic text-center text-[#573319]">
+                  {ATTIRE_FOR_GUESTS.gentlemen.subDescription}
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row w-full justify-around my-6">
+              {ATTIRE_FOR_GUESTS.colors.map((color, i) => {
+                const colors = ['bg-[#F0E8DB]', 'bg-[#E2D4C5]', 'bg-[#D8C9BA]'];
+                const className = `${colors[i]} w-1/4 pt-[25%]`;
+                return <div className={className} />;
+              })}
+            </div>
+            <span className="text-base italic text-center text-[#573319]">
+              {ATTIRE_FOR_GUESTS.note}
+            </span>
+          </div>
+          <div className="flex flex-col w-full lg:w-[45%] mb-20 lg:mb-0 justify-center ">
+            <h2 className="font-playfair text-center fade-up text-[#573319] font-playfair text-4xl lg:text-[3rem] leading-[100%]">
+              {ATTIRE_FOR_GUESTS.title}
+            </h2>
+          </div>
+        </div>
+      </ColoredBgWrapper>
+
+      {/* <ColoredBgWrapper bgColor="nata">
 				<div
 					id="entourage"
 					className="flex flex-col lg:flex-row w-full max-w-[1920px] mx-auto gap-x-20 px-4 lg:px-32 py-10 lg:py-10 fade-up">
@@ -249,6 +266,6 @@ export default function TheWedding() {
 					</div>
 				</div>
 			</ColoredBgWrapper> */}
-		</Layout>
-	);
+    </Layout>
+  );
 }
