5c3931e9477be0fb5b60ee8de63da01c186fade2
DCrisanto <gdcrisanto@gmail.com>
2025-03-09 14:09:09 +0800

    fix: edit FAQS

--- lib/constants/faqs.ts
+++ lib/constants/faqs.ts
@@ -1,69 +1,67 @@
 import faqsHero from '../../public/assets/faqs_hero.jpg';
 
 const FAQS_HERO = {
-	image: faqsHero,
-	title: 'Frequently Asked Questions',
+  image: faqsHero,
+  title: 'Frequently Asked Questions',
 };
 
 const FAQS_INFO = [
-	{
-		id: 'FAQ1',
-		question: 'How do I RSVP and by when?',
-		answer: `To RSVP, click the RSVP button in the website header and complete the form.
-
-Submit your response by <b style="color:#573319;">February 28, 2025</b>. Any RSVPs received after this date will be considered a <i><b style="color:#573319;">no</b></i>.
+  {
+    id: 'FAQ1',
+    question: 'How do I RSVP?',
+    answer: `To RSVP, click the RSVP button in the website header and complete the form.
 
 If you are unable to attend, kindly let us know as soon as possible.
 
 For inquiries regarding our wedding day, you may contact our coordinator, Maine Events, at +639618041109.
 `,
-	},
-	{
-		id: 'FAQ2',
-		question: 'Can I bring a plus one?',
-		answer: `Since we are keeping our wedding intimate, we can only accommodate guests who are formally invited. If your invitation includes a plus one, they will be listed when you RSVP.`,
-	},
-// 	{
-// 		id: 'FAQ3',
-// 		question: 'Can we bring our kids?',
-// 		answer: `While we adore your little ones, we have decided to keep our ceremony and reception adults-only, with the exception of our flower girls, who are part of the entourage.
+  },
+  {
+    id: 'FAQ2',
+    question: 'Can I bring a plus one?',
+    answer: `Since we are keeping our wedding intimate, we can only accommodate guests who are formally invited. If your invitation includes a plus one, they will be listed when you RSVP.`,
+  },
+  // 	{
+  // 		id: 'FAQ3',
+  // 		question: 'Can we bring our kids?',
+  // 		answer: `While we adore your little ones, we have decided to keep our ceremony and reception adults-only, with the exception of our flower girls, who are part of the entourage.
 
-// We appreciate your understanding and hope you can make arrangements to celebrate with us.`,
-// 	},
-	{
-		id: 'FAQ4',
-		question: 'Is there a dress code?',
-		answer: `Yes, we would love to see our family and friends dressed elegantly for our special day. The dress code is strictly formal in beige, ecru or taupe.
+  // We appreciate your understanding and hope you can make arrangements to celebrate with us.`,
+  // 	},
+  {
+    id: 'FAQ4',
+    question: 'Is there a dress code?',
+    answer: `Yes, we would love to see our family and friends dressed elegantly for our special day. The dress code is strictly formal in beige, ecru or taupe.
 
 Gentlemen: Long-sleeved, classic beige Barong Tagalog with black trousers
 
 Ladies: Floor-length evening gown
 
 For more details, visit <b style="color:#573319;">The Wedding</b> page.`,
-	},
-	{
-		id: 'FAQ5',
-		question: 'Do you have a gift registry?',
-		answer: `We are excited to celebrate our wedding with you and your presence is the best gift we could ask for. If you wish to give a gift, we would be deeply grateful for contributions towards our honeymoon or future home.`,
-	},
-	{
-		id: 'FAQ6',
-		question: 'Is there available parking at the venue?',
-		answer: `Yes, complimentary parking is available for guests at the church and the reception venue. For your safety, avoid leaving valuables in your vehicles.`,
-	},
-	{
-		id: 'FAQ7',
-		question: 'Are there nearby hotels?',
-		answer: `Yes, there are several hotels located near the reception venue. We have provided a few recommendations in the <b style="color:#573319;">Venue</b> page for your convenience. 
+  },
+  {
+    id: 'FAQ5',
+    question: 'Do you have a gift registry?',
+    answer: `We are excited to celebrate our wedding with you and your presence is the best gift we could ask for. If you wish to give a gift, we would be deeply grateful for contributions towards our honeymoon or future home.`,
+  },
+  {
+    id: 'FAQ6',
+    question: 'Is there available parking at the venue?',
+    answer: `Yes, complimentary parking is available for guests at the church and the reception venue. For your safety, avoid leaving valuables in your vehicles.`,
+  },
+  {
+    id: 'FAQ7',
+    question: 'Are there nearby hotels?',
+    answer: `Yes, there are several hotels located near the reception venue. We have provided a few recommendations in the <b style="color:#573319;">Venue</b> page for your convenience. 
 
 We have available rooms for wedding guests at <i><b style="color:#573319;">Anya Resorts Tagaytay</b></i>. Mention the <i><b style="color:#573319;">Cano–Crisanto Wedding</b></i> when booking to enjoy discounted room rates.
 
 For room reservations at Anya, send an email directly to gemalyne@ahg.com.ph / jhen@ahg.com.ph / events@ahg.com.ph.
 `,
-	},
+  },
 ];
 
 export default {
-	FAQS_HERO,
-	FAQS_INFO,
+  FAQS_HERO,
+  FAQS_INFO,
 };
